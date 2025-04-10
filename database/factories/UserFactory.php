f30331532265a889a827dbed2d65affc4ca527d5
Ivan Rizkyanto <ivanzkyanto@outlook.com>
2025-04-10 17:36:04 +0700

    Add user seeders

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,6 +26,7 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
