0690034c44e3753d161e0197bd0fe9cccbd40d56
Ivan Rizkyanto <ivanzkyanto@outlook.com>
2025-04-16 17:48:58 +0700

    Implement projects page

--- resources/js/pages/project/index.tsx
+++ resources/js/pages/project/index.tsx
@@ -1,36 +1,90 @@
 import MainLayout from "@/layouts/main-layout";
+import { Project } from "@/types";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import CardActions from '@mui/material/CardActions';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import { router } from "@inertiajs/react";
 
-export default function Index() {
+interface IndexProps {
+  projects: Project[];
+}
+
+export default function Index({ projects }: IndexProps) {
   return (
     <MainLayout>
-      <Typography sx={{ marginBottom: 2 }}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-        enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-        imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-        Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-        Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-        adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-        nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-        leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-        feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-        consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-        sapien faucibus et molestie ac.
-      </Typography>
-      <Typography sx={{ marginBottom: 2 }}>
-        Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-        eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-        neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-        tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-        sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-        tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-        gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-        et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-        tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-        eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-        posuere sollicitudin aliquam ultrices sagittis orci a.
-      </Typography>
+      <Box component="section">
+        <Typography variant="h4" gutterBottom>Projects</Typography>
+        <Grid
+          container
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 1, sm: 2, md: 3, lg: 4, xl: 6 }}
+        >
+          {projects.map((project) => (
+            <Grid key={project.id} size={{ xs: 1 }}>
+              <Card sx={{ height: '10rem', display: 'flex', flexDirection: 'column' }}>
+                <Box sx={{ height: '0.5rem', backgroundColor: project.color_hex }} />
+                <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+                  <CardContent>
+                    <Typography
+                      gutterBottom
+                      variant="h6"
+                      component="div"
+                      sx={{
+                        overflow: 'hidden',
+                        textOverflow: 'ellipsis',
+                        display: '-webkit-box',
+                        WebkitLineClamp: 1,
+                        WebkitBoxOrient: 'vertical',
+                        lineHeight: '1.25rem',
+                        minHeight: '1.5rem',
+                      }}
+                    >
+                      {project.name}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      sx={{
+                        color: 'text.secondary',
+                        overflow: 'hidden',
+                        textOverflow: 'ellipsis',
+                        display: '-webkit-box',
+                        WebkitLineClamp: 2,
+                        WebkitBoxOrient: 'vertical',
+                        lineHeight: '1.25rem',
+                        minHeight: '2.5rem',
+                      }}
+                    >
+                      {project.description}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button
+                      size="small"
+                      color="primary"
+                      onClick={() => {
+                        router.get(route('projects.show', project.id))
+                      }}
+                    >
+                      Open
+                    </Button>
+                  </CardActions>
+                </Box>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+      <Box sx={{ position: 'fixed', bottom: '1rem', right: '1rem' }}>
+        <Fab color="primary" aria-label="add">
+          <AddIcon />
+        </Fab>
+      </Box>
     </MainLayout>
   );
 }
